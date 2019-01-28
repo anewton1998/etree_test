90ca051793b8da7cb8ddac199d96e239e4e75355
andy <andy@hxr.us>
2019-01-28 11:36:20 -0500

    added tests for networks

--- src/test/kotlin/com/rcode3/etree_test/BasicsTest.kt
+++ src/test/kotlin/com/rcode3/etree_test/BasicsTest.kt
@@ -32,6 +32,37 @@ class BasicsTest : ShouldSpec( {
 
         val map = NestedIntervalMap<IpRange,String>( IpResourceIntervalStrategy.getInstance() )
 
+        val net10  = IpRange.parse("10.0.0.0/24")
+        val net20 = IpRange.parse("20.0.0.0/24")
+
+        map.put( net10, "net10" )
+        map.put( net20, "net20" )
+
+        map.findExact( IpRange.parse( "10.0.0.0/24" ) ) shouldBe "net10"
+        map.findExact( IpRange.parse( "20.0.0.0/24" ) ) shouldBe "net20"
+
+        map.findExactOrFirstLessSpecific( IpRange.parse( "10.0.0.0/32" ) ) shouldBe "net10"
+        map.findExactOrFirstLessSpecific( IpRange.parse( "20.0.0.0/32" ) ) shouldBe "net20"
+
+    }
+
+    should( "build nested networks" ) {
+
+        val map = NestedIntervalMap<IpRange,String>( IpResourceIntervalStrategy.getInstance() )
+
+        val net10_16 = IpRange.parse("10.0.0.0/16")
+        val net10_24 = IpRange.parse("10.0.0.0/24")
+
+        map.put(net10_16, "net10_16" )
+        map.put(net10_24, "net10_24" )
+
+        map.findExact( IpRange.parse( "10.0.0.0/16" ) ) shouldBe "net10_16"
+        map.findExact( IpRange.parse( "10.0.0.0/24" ) ) shouldBe "net10_24"
+
+        map.findExactOrFirstLessSpecific( IpRange.parse( "10.0.0.0/32" ) ) shouldBe "net10_24"
+        map.findExactOrFirstLessSpecific( IpRange.parse( "10.0.0.0/24" ) ) shouldBe "net10_24"
+        map.findExactOrFirstLessSpecific( IpRange.parse( "10.0.0.0/19" ) ) shouldBe "net10_16"
+        map.findExactOrFirstLessSpecific( IpRange.parse( "10.0.0.0/16" ) ) shouldBe "net10_16"
     }
 
 })
