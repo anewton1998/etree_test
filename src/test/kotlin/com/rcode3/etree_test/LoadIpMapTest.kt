1037773a881a337a23a3eca1a5df53f7063fd1f1
andy <andy@arin.net>
2019-02-01 07:17:28 -0800

    reworked loadipmaptest

--- src/test/kotlin/com/rcode3/etree_test/LoadIpMapTest.kt
+++ src/test/kotlin/com/rcode3/etree_test/LoadIpMapTest.kt
@@ -1,27 +1,40 @@
 package com.rcode3.etree_test
 
+import io.kotlintest.Spec
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.ShouldSpec
 import net.ripe.ipresource.IpRange
+import java.io.File
 import java.io.File.createTempFile
 
 /**
  * Does a basic test of loading a map for a JSON Lines file.
  */
-class LoadIpMapTest : ShouldSpec ( {
+class LoadIpMapTest : ShouldSpec () {
 
-    should( "parse loadipmaptest.jsonlines" ) {
+    var tmpFile : File? = null
 
+    override fun beforeSpec(spec: Spec) {
         //copy the resource to a temp location
         val classLoader = javaClass.classLoader
         val resource = classLoader.getResource( "loadipmaptest.jsonlines" )
         val data = resource.readText()
-        val file = createTempFile( "loadipmaptest", "jsonlines" )
-        file.writeText( data )
-
-        val map = loadIpMapFromJsonLines( file.absolutePath )
-        map.findExact( IpRange.parse( "2610:00E0:0000:0000:0000:0000:0000:0000/32" ) ) shouldBe "NET6-2610-E0-1"
-        map.findExact( IpRange.parse( "209.136.123.000/24" ) ) shouldBe "NET-209-136-123-0-1"
+        tmpFile = createTempFile( "loadipmaptest", "jsonlines" )
+        tmpFile?.writeText( data )
     }
 
-})
+    override fun afterSpec(spec: Spec) {
+        tmpFile?.delete()
+    }
+
+    init {
+        should( "parse loadipmaptest" ) {
+
+            val map = loadIpMapFromJsonLines( tmpFile?.absolutePath ?: "no tmpFile" )
+            map.findExact( IpRange.parse( "2610:00E0:0000:0000:0000:0000:0000:0000/32" ) ) shouldBe "NET6-2610-E0-1"
+            map.findExact( IpRange.parse( "209.136.123.000/24" ) ) shouldBe "NET-209-136-123-0-1"
+
+        }
+    }
+
+}
